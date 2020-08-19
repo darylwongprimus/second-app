a3dbd51d48bec7c654cbaf77d6a9b115267d0e20
daryl wong <darylwong@primustech.com.sg>
2020-08-19 17:04:45 +0800

    second

---
+++ pages/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news</h1>
+    </section>
+    <section class="featured-posts">
+      <PostPreview
+        id="1"
+        thumbnail="https://images.unsplash.com/photo-1579783483458-83d02161294e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"
+        title="Hello there"
+        previewText="this is my first post"
+      />
+      <PostPreview
+        id="2"
+        thumbnail="https://images.unsplash.com/photo-1579783483458-83d02161294e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"
+        title="Hello there"
+        previewText="this is my second post"
+      />
+      <PostPreview
+        id="3"
+        thumbnail="https://images.unsplash.com/photo-1579783483458-83d02161294e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60"
+        title="Hello there"
+        previewText="this is my third post"
+      />
+      <PostPreview />
+      <PostPreview />
+    </section>
+  </div>
+</template>
+
+<script>
+import PostPreview from "@/components/Posts/PostPreview";
+export default {
+  components: {
+    PostPreview,
+  },
+};
+</script>
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+  background-image: url("~assets/images/background-image.jpg");
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
